2a79c3163c02c5a99a492256fa8968161b2c8ba1
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-02-20 06:49:02 -0300

    Refactor: muda tipagem função runBattles

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import Character from './Character';
-import Monster from './Monster';
-import Dragon from './Dragon';
 import Battle, { PVP, PVE } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
 
-const player1 = new Character('Player1');
-const player2 = new Character('Player2');
-const player3 = new Character('Player3');
+const player1 = new Character('P1');
+const player2 = new Character('P2');
+const player3 = new Character('P3');
 
 player1.levelUp();
 player1.levelUp();
@@ -17,7 +17,7 @@ const monster2 = new Dragon();
 const pvp = new PVP(player2, player3);
 const pve = new PVE(player1, [monster1, monster2]);
 
-const runBattles = (listBattles: Battle[]) =>
-  listBattles.forEach((b) => b.fight);
+const runBattles = (battles: Battle[]): void =>
+  battles.forEach((battle: Battle) => battle.fight());
 
 export { player1, player2, player3, monster1, monster2, pvp, pve, runBattles };
